fbc9495fc9a99d3b413cc09eab344c46e5dd1015
POISON <86126808+Cat-of-Tg@users.noreply.github.com>
2021-12-18 21:50:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ Bot = Client(
     api_hash = os.environ["API_HASH"]
 )
 
+ABOUT_TXT = os.environ["ABOUT_TXT"]
+
 START_MSG = """Hey {}
 i am Poisons Assistant 
 You Can Contact My Master From Here
@@ -33,7 +35,7 @@ async def start(bot, update):
 @Bot.on_message(filters.private & filters.command(["about"]))
 async def start(bot, update):
     await update.reply_text(
-        text=ABOUT_MSG.format(update.from_user.mention),
+        text=ABOUT_TXT.format(update.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=START_BTN
     )
