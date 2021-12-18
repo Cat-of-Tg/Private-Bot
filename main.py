52b417a78ca5ba2c7c272fe15f27a6678388e745
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-12-18 19:53:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-bot = Client(
+Bot = Client(
     "private bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
@@ -24,8 +24,8 @@ START_BTN = InlineKeyboardMarkup(
 @Bot.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
     await update.reply_text(
-        text=START_MSG,
+        text=START_MSG.format(update.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=START_BTN
     )
-bot.run()
+Bot.run()
