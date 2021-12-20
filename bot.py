2556eae2f0bfe0515d1ab32926cbbfdc53ef47d8
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-12-20 06:06:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,13 +15,19 @@ START_MSG = """Hey {}
 i am Poisons Assistant 
 You Can Contact My Master From Here
 """
-ABOUT_MSG = """ SOURCE - github.com/Cat-of-tg/private-bot """
+ABOUT_MSG = """ 👀Source code
+- owo it seems to great you have asked for source code. 
+[CLICK here to get source code](https://github.com/Cat-of-Tg/Private-Bot). 
+"""
 
 START_BTN = InlineKeyboardMarkup(
         [[
         InlineKeyboardButton('JOIN HERE', url= 'https://t.me/Team_Lad'),
-        InlineKeyboardButton('SOURCE', callback_data = about)
-        ]]
+        InlineKeyboardButton('SOURCE', callback_data='about') 
+    )
+ABOUT_BTN = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('JOIN HERE', callback_data='home'
     )
 
 @Bot.on_message(filters.private & filters.command(["start"]))
@@ -32,5 +38,19 @@ async def start(bot, update):
         reply_markup=START_BTN
     )
 
-
+@Bot.on_callback_query()
+async def cb_handler(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_MSG.format(update.from_user.mention),
+            reply_markup=START_BTN,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_MSG,
+            reply_markup=ABOUT_BTN,
+            disable_web_page_preview=True
+        )
+    
 Bot.run()
